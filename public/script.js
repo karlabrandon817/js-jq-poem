7f0bf6522e298e8451e69b9536f987226533d19c
Karla Hugo <karlabrandon817@gmail.com>
2017-01-05 15:40:28 -0600

    updated alert

--- public/script.js
+++ public/script.js
@@ -1,23 +1,24 @@
-var hughesPoem = [ 'Like','a','welcome','summer','rain,','humor',
-  'may','suddenly','cleanse','and','cool','the','earth,','the','air',
-  'and','you.'];
+var hughesPoem = ['Like', 'a', 'welcome', 'summer', 'rain,', 'humor',
+    'may', 'suddenly', 'cleanse', 'and', 'cool', 'the', 'earth,', 'the', 'air',
+    'and', 'you.'
+];
 var currentIndex = 0;
 
-$(document).ready(function(){
-  $('#poemButton').on('click',function(){
-    addWord(hughesPoem);
-  });//end poemButton click function
-});//end doc ready function
+$(document).ready(function() {
+    $('#poemButton').on('click', function() {
+        addWord(hughesPoem);
+    }); //end poemButton click function
+}); //end doc ready function
 
-var addWord = function(array){
-  var i = (currentIndex++) % array.length;
-  if(currentIndex == array.length+1){
-    currentIndex = 0;
-    $('#poemDiv').html('');
-    alert("You have reached the end of the poem...Click button to start over.");
-     //sets index to 0 and clears DOM when end of poem is reached
-  }//end if
-  else {
-    $('#poemDiv').append(array[i] + ' ');
- }//end else
-};//end addWord function
+var addWord = function(array) {
+    var i = (currentIndex++) % array.length;
+    if (currentIndex == array.length + 1) {
+        currentIndex = 0;
+        $('#poemDiv').html('');
+        alert("You have reached the end of the poem...Click \'ok'\ to start over.");
+        //sets index to 0 and clears DOM when end of poem is reached
+    } //end if
+    else {
+        $('#poemDiv').append(array[i] + ' ');
+    } //end else
+}; //end addWord function
